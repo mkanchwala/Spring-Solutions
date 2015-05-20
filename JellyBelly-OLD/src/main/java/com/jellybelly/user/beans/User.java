b45066c4263a340d4a53c4699e4a7b4d1bb0ac55
mkanchwala <mkanchwala@mkanchwala>
2015-05-20 10:44:25 +0530

    Pushing it to GIT

---
+++ JellyBelly-OLD/src/main/java/com/jellybelly/user/beans/User.java
@@ -0,0 +1,131 @@
+package com.jellybelly.user.beans;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ * @author mkanchwala
+ */
+@Entity
+@Table(name = "user")
+public class User extends BaseBean {
+
+	private static final long serialVersionUID = 7427622175146338719L;
+	private Integer userId;
+	private String username;
+	private String password;
+	private Boolean isEnabled;
+	private Boolean isVerified;
+	private Integer smsCode;
+	private Date dateCreated;
+	private Date lastUpdated;
+	private String createdBy;
+	private String lastUpdatedBy;
+
+	public User() {
+	}
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "user_id", unique = true, nullable = false)
+	public Integer getUserId() {
+		return this.userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	@Column(name = "username", length = 256)
+	public String getUsername() {
+		return this.username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	@Column(name = "password", nullable = false)
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Column(name = "is_enabled")
+	public Boolean getIsEnabled() {
+		return this.isEnabled;
+	}
+
+	public void setIsEnabled(Boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	@Column(name = "is_verified")
+	public Boolean getIsVerified() {
+		return this.isVerified;
+	}
+
+	public void setIsVerified(Boolean isVerified) {
+		this.isVerified = isVerified;
+	}
+
+	@Column(name = "sms_code")
+	public Integer getSmsCode() {
+		return this.smsCode;
+	}
+
+	public void setSmsCode(Integer smsCode) {
+		this.smsCode = smsCode;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "date_created", nullable = false, length = 19)
+	public Date getDateCreated() {
+		return this.dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "last_updated", length = 19)
+	public Date getLastUpdated() {
+		return this.lastUpdated;
+	}
+
+	public void setLastUpdated(Date lastUpdated) {
+		this.lastUpdated = lastUpdated;
+	}
+
+	@Column(name = "created_by", length = 256)
+	public String getCreatedBy() {
+		return this.createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	@Column(name = "last_updated_by", length = 256)
+	public String getLastUpdatedBy() {
+		return this.lastUpdatedBy;
+	}
+
+	public void setLastUpdatedBy(String lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
+	}
+
+}
---
+++ JellyBelly-OLD/src/main/java/com/jellybelly/user/service/UserService.java
@@ -0,0 +1,8 @@
+package com.jellybelly.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
